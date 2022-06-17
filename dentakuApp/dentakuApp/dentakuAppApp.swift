fb472e680913f9abb0f9a03ada6e2b08dfca5c3d
AoOhashi <107658795+AoOhashi@users.noreply.github.com>
2022-06-17 10:26:52 +0900

    Add files via upload
    ポートフォリオ4

---
+++ dentakuApp/dentakuApp/dentakuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  dentakuAppApp.swift
+//  dentakuApp
+//
+//  Created by Uske on 2021/11/07.
+//
+
+import SwiftUI
+
+@main
+struct dentakuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
